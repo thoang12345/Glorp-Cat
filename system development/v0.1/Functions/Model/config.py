3ab9232d7034b84b27a380f70d2e0febcf9a030a
thoang12345 <thienanhoang@student.midlandstech.edu>
2026-08-15 17:54:03 -0400

    added requirements text file and updated model
    Using Nemotron now

--- system development/v0.1/Functions/Model/config.py
+++ system development/v0.1/Functions/Model/config.py
@@ -1,8 +1,9 @@
-MODEL_NAME = "gemma4:26b"
+MODEL_NAME = "nemotron-3.5-lightning-fixed:30b"
+#MODEL_NAME = "gemma4:26b"
 CHATBOT_NAME = "GlorpCat"
 RERANKER_MODEL = "BAAI/bge-reranker-base"
 
-CHROMADB_PATH = "/home/thienan/Documents/Github/PaperParsing/Docling Parsing/v0.2/ChromaDB"
+CHROMADB_PATH = "/home/thienan/Documents/GitHub/PaperParsing/Docling Parsing/v0.2/ChromaDB"
 
 STREAM = True
 THINKING = True
